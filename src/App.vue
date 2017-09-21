5f1faca3a413850e6ffb1617c894b14a7de667c4
Wallison Santos <wallison@outlook.com>
2017-09-21 20:18:36 -0300

    Add initial commit
    Added initial commit. I'm using vue pwa scaffolding template.
    
    more info here: https://github.com/vuejs-templates/pwa

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+    <main>
+      <img src="./assets/logo.png" alt="Vue.js PWA">
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+
+import appHeader from './components/appheader';
+
+export default {
+  name: 'app',
+  components: {
+    appHeader,
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+main {
+  text-align: center;
+  margin-top: 40px;
+}
+
+header {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+  background-color: #35495E;
+  color: #ffffff;
+}
+
+header span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: .02em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <span>{{appName}}</span>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'app-header',
+  data() {
+    return {
+      appName: 'Um dia, uma música',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
